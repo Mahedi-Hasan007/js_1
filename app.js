7a3a8c49466cb89305913f950803f4c94b73062c
unknown <mahedihasan1691@gmail.com>
2022-12-20 22:41:06 +0600

    initilize project

---
+++ app.js
@@ -0,0 +1,72 @@
+/*console.log("This is first test");
+document.write('shift something');
+output.innerHTML ="<u>This is one of </u>This is our first document" ;
+output.innerHTML=123454;*/
+
+// number, string, list, object
+//template string
+
+/*var a = 10;
+var b = 20;
+var test = "This is a security" + a + " " + b;
+var zuri = {
+    name: "Mahedi Hasan",
+    roll: 30,
+    address:"Mirpur",
+    suject: [
+        'bangla',
+        'english',
+        'math',
+        'islam',
+    ]
+    image: 'https://cdn.onlinewebfonts.com/svg/img_329115.png'
+}; 
+
+var list =[
+    'red',
+    'yellow',
+    'blue',
+    452,
+    458,
+    236,
+    235,
+    {
+        name:'lime',
+        address:'new-york',
+    },
+    [
+        'riger',
+        'cat',
+        'first',
+    ]
+    {
+        name:'second',
+        roll:'12584',
+
+    }
+]
+
+/*output.innerHTML = zuri;*/
+/*console.log(zuri.image);*/
+
+
+
+// number, string, list, object
+//template string
+
+// integer, float, string, array, object, null, boolean, undefined
+
+
+//+ - / * %
+sum.onclick = function(){
+    output.innerHTML = +first_number.value + +second_number.value;
+    console.log('summation done');
+}
+div.onclick = function(){
+    output.innerHTML = +first_number.value / +second_number.value;
+    console.log('division done');
+}
+mul.onclick = function(){
+    output.innerHTML = +first_number.value * +second_number.value;
+    console.log('multiplication done');
+}
